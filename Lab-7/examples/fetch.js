e25bc3b36baa34b3401c2a5ae3f5efaf6a00e60c
cs <siwon.cezary@gmail.com>
2023-01-21 12:50:28 +0100

    lab-7 exercise

--- Lab-7/examples/fetch.js
+++ Lab-7/examples/fetch.js
@@ -1,13 +1,18 @@
-fetch("https://jsonplaceholder.typicode.com/photos", {
+fetch("https://jsonplaceholder.typicode.com/photoss", {
     method: 'GET',
     headers: {
         'Accept': 'application/json'
     }
 })
-    .then(response => response.json())
+    .then(response => {
+        if(response.ok) {
+           return response.json()
+        }
+        throw "no content";
+    })
     .then(json => {
         console.dir(json)
     })
     .catch(e => {
-        console.log("error");
+        console.log(e);
     });
