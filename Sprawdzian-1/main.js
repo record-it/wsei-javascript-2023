c7c4659375dab06dead98e1877398e9981ca966e
cs <siwon.cezary@gmail.com>
2023-02-18 11:58:12 +0100

    sprawdzian

---
+++ Sprawdzian-1/main.js
@@ -0,0 +1,10 @@
+/**
+ * Za pomoca fetch pobierz listę postów z api o adresie https://jsonplaceholder.typicode.com/posts
+ * W każdym z postów znajduje się userId, które określa dane użytkownika pod adresem https://jsonplaceholder.typicode.com/users/userId
+ * Napisz skrypt, który po pobraniu listy postów i użytkowników wygeneruje dokument html z tabelą o trzech kolumnach
+ * nazwa użytkownka - name
+ * tytuł posta - title
+ * treść posta - body
+ * Nazwa użytkownika musi być linkiem lub przyciskiem, który po kliknięciu otwiera nowy dokument z wszystkimi danymi użytkownika i linkiem powrotnym
+ * do strony z postami.
+ */
