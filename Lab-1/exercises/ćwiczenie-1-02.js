61e94769f306ae00dc543899383def7879648120
cs <siwon.cezary@gmail.com>
2023-09-28 13:34:31 +0200

    lab-1 fixed

--- Lab-1/exercises/ćwiczenie-1-02.js
+++ Lab-1/exercises/ćwiczenie-1-02.js
@@ -1,12 +1,16 @@
 /**
  * Napisz skrypt, który w łańcuchu triangle generuje ciąg znaków '#' i '\n' tworzących kształ trójkąta o wysokości 
- * w zmiennej height. Po wyświetleniu na konsoli powinien zostać wyświetlony poniższy wzór liczba wierzy powinna odpowiadać 
+ * w zmiennej height. Po wyświetleniu na konsoli powinien zostać wyświetlony poniższy wzór liczba wierszy powinna odpowiadać
  * zmiennej height:
  * #
  * ##
  * ###
  * ####
- * ##### 
+ * #####
+ * Uruchom skrypt dla różnych wartości height np:
+ * height = 1
+ * height = 0
+ * height = 10
  */
 let height = 5;
 let triangle = '';
--- Lab-1/exercises/ćwiczenie-1-03.js
+++ Lab-1/exercises/ćwiczenie-1-03.js
@@ -1,7 +1,7 @@
 /**
- * Napisz skrypt zamieniający wartość zmiennej decimalNumber  na liczbę rzemską w zakresie od 1 do 10.
+ * Napisz skrypt zamieniający wartość zmiennej decimalNumber  na liczbę rzymską w zakresie od 1 do 10.
  * Wynik zapisz do zmienej romanNumber.
- * Jeśli decimalNumber nie mieści sie w zakresie to umieść w romanNumber komunikat: Decimal number is out of range!
+ * Jeśli decimalNumber nie zmieści sie w zakresie to umieść w romanNumber komunikat: Decimal number is out of range!
  * Jeśli decimalNumber jest równe undefined lub null to umieść w romanNumber komunikat: Decimal number is undefined or null!
  */
 let decimalNumber = 4;
